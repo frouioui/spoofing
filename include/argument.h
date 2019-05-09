c5c8cd493a0b45eeb27978d18ee3f5931593c89a
Florent Poinsard <florent.poinsard@epitech.eu>
2019-05-09 16:32:26 +0200

    [ADD] - Addition of the main argument struct #2

--- include/argument.h
+++ include/argument.h
@@ -8,6 +8,17 @@
 #if !defined(_ARGUMENT_H)
 #define _ARGUMENT_H
 
+#include <stdbool.h>
 
+struct argument_s {
+    char *src_ip;
+    char *dest_ip;
+    char *interface;
+    bool print_broadcast;
+    bool print_spoof;
+    char *mac_address_spoof;
+};
+
+typedef struct argument_s argument_t;
 
 #endif // _ARGUMENT_H
